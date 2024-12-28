ed4bf00461957d70aa974b2fd1435bdc10e82975
Alekhya <alekhya.nallabati@gmail.com>
2024-12-28 23:52:39 +0530

    Inducing mongodb

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
-
+client = MongoClient("mongodb+srv://alekhyanallabati:Alekhya123@microblog-application.szn8h.mongodb.net/")
+app.db = client.microblog
 entries = []
 
 @app.route("/", methods=["GET", "POST"])
@@ -12,6 +14,7 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%y-%m-%d")
         entries.append((entry_content,formatted_date))
+        app.db.entries.insert_one({"content":entry_content, "date":formatted_date})
 
     entries_with_date = [
         (
@@ -20,6 +23,6 @@ def home():
             datetime.datetime.strptime(entry[1],"%y-%m-%d").strftime("%b %d")
         )
             for entry in entries
-        ]
+    ]
 
     return render_template("home.html", entries=entries_with_date)
